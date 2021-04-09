27640eba53a3198f2ea7e919e798f66f6cddf063
kent <kntchn@gmail.com>
2021-04-09 15:59:23 -0400

    halfway

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let taskForm = document.querySelector("#create-task-form");
+  let taskUl = document.querySelector("#tasks");
+  // console.log(taskForm)
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+   
+
+    let form = e.target  
+    let inputBox = form["new-task-description"]
+    let whatUserTyped = inputBox.value
+    // console.log(whatUserTyped)
+
+    //DOM Manipulation
+    let blankLi = document.createElement("li")
+
+    blankLi.innerText = whatUserTyped;
+    blankLi.className = "inputtext";
+    blankLi.innerHTML = "<button> Delete</button>"
+
+    taskUl.append(blankLi);
+
+
+
+  })
 });
